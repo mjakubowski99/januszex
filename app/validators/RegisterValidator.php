dc595d447836cd43cca51d93742fb3603a2d5010
mjakubowski99 <48959665+mjakubowski99@users.noreply.github.com>
2021-03-15 18:30:00 +0100

    Update RegisterValidator.php

--- app/validators/RegisterValidator.php
+++ app/validators/RegisterValidator.php
@@ -46,18 +46,18 @@ class RegisterValidator{
 
     public function validate($data){
         if( $this->fieldsAreEmpty($data) ) return "Wypełnij wszystkie pola";
-        if( !$this->mailValid($data['txt_email']) ) return "Podaj prawidłowy adres email";
-        if( !$this->lengthValid($data['txt_password'], 8) ) return "Hasło jest za krótkie";
-        if( !$this->passwordsAreTheSame($data['txt_password'], $data['txt_confirm']) ) return "Hasła się nie zgadzają";
-        if( $this->lengthValid($data['txt_name'], 30) ) return "Imie jest za długie";
-        if( $this->lengthValid($data['txt_surname'], 30) ) return "Nazwisko jest za długie";
-        if( $this->lengthValid($data['txt_city'], 30) ) return "Nazwa miasta jest za długa";
-        if( $this->lengthValid($data['txt_street'], 30) ) return "Nazwa ulicy jest za długa";
-        if( !$this->stringNumeric($data['txt_home_number']) ) return "Podaj wartość numeryczną numeru domu";
-        if( !$this->stringNumeric($data['txt_flat_number']) ) return "Podaj wartość numeryczną numeru mieszkania";
-        if( $this->lengthValid($data['txt_postoffice_name'], 30) ) return "Nazwa poczty jest za długa";
-        if( !$this->postalCodeValid($data['txt_postoffice_code']) ) return "Kod pocztowy jest nieprawidłowy, prawidłowy format to dd-ddd";
-        if( $this->userExists( $data['txt_email'] ) ) return "Na podany adres e-mail zostało założone już konto. Spróbuj opcji odzyskiwania hasła";
+        if( !$this->mailValid($data['email']) ) return "Podaj prawidłowy adres email";
+        if( !$this->lengthValid($data['password'], 8) ) return "Hasło jest za krótkie";
+        if( !$this->passwordsAreTheSame($data['password'], $data['txt_confirm']) ) return "Hasła się nie zgadzają";
+        if( $this->lengthValid($data['name'], 30) ) return "Imie jest za długie";
+        if( $this->lengthValid($data['surname'], 30) ) return "Nazwisko jest za długie";
+        if( $this->lengthValid($data['city'], 30) ) return "Nazwa miasta jest za długa";
+        if( $this->lengthValid($data['street'], 30) ) return "Nazwa ulicy jest za długa";
+        if( !$this->stringNumeric($data['home_number']) ) return "Podaj wartość numeryczną numeru domu";
+        if( !$this->stringNumeric($data['flat_number']) ) return "Podaj wartość numeryczną numeru mieszkania";
+        if( $this->lengthValid($data['postoffice_name'], 30) ) return "Nazwa poczty jest za długa";
+        if( !$this->postalCodeValid($data['postoffice_code']) ) return "Kod pocztowy jest nieprawidłowy, prawidłowy format to dd-ddd";
+        if( $this->userExists( $data['email'] ) ) return "Na podany adres e-mail zostało założone już konto. Spróbuj opcji odzyskiwania hasła";
 
         return true;
     }
