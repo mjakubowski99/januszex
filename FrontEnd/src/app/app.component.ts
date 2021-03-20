fcbe7e69ac176790a847afe7b7fcd0d110a683c7
Piotr Korniak <piotrkorniak99@gmail.com>
2021-03-20 21:51:20 +0100

    add token interceptor

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
+  public ngOnInit(): void {
+    this.authenticationService.autoLogin();
+    if (this.authenticationService.user.value) {
+      console.log('');
+      console.log('');
+      console.log('');
+      console.log(this.authenticationService.user.value.email);
+      console.log(this.authenticationService.user.value.token);
+      console.log('');
+      console.log('');
+      console.log('');
+    }
+  }
 }
--- FrontEnd/src/app/header/header.component.ts
+++ FrontEnd/src/app/header/header.component.ts
@@ -1,25 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService} from '../services/authentication.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-  constructor() {
+  constructor(private authenticationService: AuthenticationService) {
     this.searchForm = new FormGroup({
       searchValue: new FormControl(null, Validators.required)
     });
   }
 
   ngOnInit(): void {
+    this.userSub = this.authenticationService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
   onSubmit(): void {
     console.log(this.searchForm);
     this.searchForm.reset();
+  }
+
+  public onLogout(): void {
+    this.authenticationService.logout();
   }
 }
